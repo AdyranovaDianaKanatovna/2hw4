941978a28b822eaec0e66fbbdf3ddc29d8fd2c3d
AdyranovaDianaKanatovna <dadyranova@edu.hse.ru>
2022-02-16 20:02:08 +0600

    Added the length comparator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,56 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static Scanner scan;
 
     public static void main(String[] args) {
         scan = new Scanner(System.in);
-        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> namesA = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             System.out.println("Enter a name to store in  a list A: ");
-            names.add(scan.next());
-            System.out.println("This is our A list :" + names);
+            namesA.add(scan.next());
+
 
         }
+        System.out.println("This is our A list :" + namesA);
+
         scan = new Scanner(System.in);
-        ArrayList<String> group = new ArrayList<>();
+        ArrayList<String> namesB = new ArrayList<>();
         for (int j = 0; j < 5; j++) {
             System.out.println("Enter a name to store in  a list B: ");
-            group.add(scan.next());
-            System.out.println("This is our B list :" + group);
-            System.out.println("____________");
+            namesB.add(scan.next());
 
-          ArrayList<String> cList = new ArrayList<>();
-          names.addAll(group);
-          System.out.println("List C:" +names);
-        }
 
+
+
+                
+            }
+        System.out.println("This is our B list :" + namesB);
+        ArrayList<String> namesC = new ArrayList<>();
+            Collections.reverse(namesC);
+            Iterator<String> iteratorA = namesA.iterator();
+            Iterator<String> iteratorB = namesB.iterator();
+            for (int i = 0; i < namesA.size(); i++) {
+                namesC.add(iteratorA.next());
+                namesC.add(iteratorB.next());
         }
-    }
+        System.out.println(namesC);
+ Collections.sort(namesC,new LegthsComparator());
+        System.out.println(namesC);
+        Collections.sort(namesC, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.length() - o1.length();
+            }
+        });
+
+        System.out.println(namesC);
+                }
+
+
+}
+
+
+
